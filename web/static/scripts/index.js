ce117cb0b4418aa09c8d636d1e3e57283de458f7
ArtuoS <arthureduardomolinari@gmail.com>
2024-07-26 22:10:58 -0300

    front-end improvement.

--- web/static/scripts/index.js
+++ web/static/scripts/index.js
@@ -1,19 +1,52 @@
 import { getCookie } from "./cookieManager.js";
 
 document.addEventListener("DOMContentLoaded", function () {
-  const button = document.getElementById("profile-button");
-  if (button == null) {
-    return;
+  const profileButton = document.getElementById("profile-button");
+  if (profileButton != null) {
+    profileButton.addEventListener("click", function (e) {
+      e.preventDefault();
+      profile(getCookie("logged_user_id"));
+    });
   }
-  
-  button.addEventListener("click", function (e) {
-    e.preventDefault();
-    profile(getCookie("logged_user_id"));
-  });
+
+  const exitButton = document.getElementById("exit-button");
+  if (exitButton != null) {
+    exitButton.addEventListener("click", function (e) {
+      e.preventDefault();
+      exit();
+    });
+  }
+
+  const booksButton = document.getElementById("books-button");
+  if (booksButton != null) {
+    booksButton.addEventListener("click", function (e) {
+      e.preventDefault();
+      books();
+    });
+  }
 });
 
 const profile = (id) => {
-    window.location.href = `/users/${id}/profile`;
+  if (id == undefined || id == null) {
+    window.location.href = `/auth`;
+    return;
+  }
+
+  window.location.href = `/users/${id}/profile`;
 };
 
-export { profile }
+const exit = () => {
+  document.cookie.split(";").forEach((cookie) => {
+    const eqPos = cookie.indexOf("=");
+    const name = eqPos > -1 ? cookie.substring(0, eqPos) : cookie;
+    document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+  });
+
+  window.location.href = `/auth`;
+};
+
+const books = () => {
+  window.location.href = `/`;
+};
+
+export { profile };
